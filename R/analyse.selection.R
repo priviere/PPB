c983232fc42bd6a3a2d31ff1ecf903cc6a856d45
gaelleVF <gaelle.van.frank@gmail.com>
2016-11-09 09:56:42 +0100

    add the possibility to plot several histograms (arg empile)

--- R/analyse.selection.R
+++ R/analyse.selection.R
@@ -9,10 +9,10 @@
 #' 
 #' @seealso \code{\link{}}
 #' 
-analyse.selection = function(res_model, data_version, variable, person, nom)
+analyse.selection = function(res_model, data_version, variable, person, nom, empile)
 {
   
-  data=unique(data_version[,c("year","location","germplasm","group","type")])
+  data=unique(data_version[,c("year","location","germplasm","group","type","modalite")])
   data=data[1:(nrow(data)/2),]
   data$vrac = paste("mu[",sapply(strsplit(as.character(data$group)," | "),function(x){return(x[[1]])}),",",data$location,":",data$year,"]",sep="")
   data$bouquet = paste("mu[",sapply(strsplit(as.character(data$group)," | "),function(x){return(x[[3]])}),",",data$location,":",data$year,"]",sep="")
@@ -33,7 +33,7 @@ analyse.selection = function(res_model, data_version, variable, person, nom)
    colnames(result) = c("MoyenneVrac","MoyenneBouquet","pvalue")
    Data=cbind(data,result)
    Data$overyielding = Data$MoyenneBouquet/Data$MoyenneVrac-1
-   Data=Data[order(Data$pvalue, decreasing =F),]
+   Data=Data[Data$modalite != "",]
    pval= NULL
    for (i in 1:nrow(Data)){
      if (as.numeric(as.character(Data[i,"pvalue"])) <= 0.01){pval = c(pval,"Significatif à 0.01")}
@@ -41,9 +41,11 @@ analyse.selection = function(res_model, data_version, variable, person, nom)
      if (as.numeric(as.character(Data[i,"pvalue"])) > 0.05){pval = c(pval,"Non significatif (pvalue >0.05)")}
    }
   Mean=mean(Data$overyielding)
+  Data$pval = pval
   
   mel = Data[Data$type %in% "Mélange", "overyielding"]
-
+ 
+  
   # Histogram
   From= min(Data$overyielding)-0.2*abs(min(Data$overyielding))
   To = max(Data$overyielding) + 0.2*max(Data$overyielding)
@@ -53,14 +55,17 @@ analyse.selection = function(res_model, data_version, variable, person, nom)
     p =  ggplot(data=Data,aes(as.numeric(as.character(overyielding)),fill=factor(pval, levels = c("Non significatif (pvalue >0.05)","Significatif à 0.05","Significatif à 0.01")))) +
     geom_histogram(breaks = seq(From,To,By), color = "black")
     }
-  
-  if (length(mel) > 0 & length(mel) < nrow(Data)) {p = p + annotate("text",label = rep("*",length(mel)),x=mel,y=0, size=10)}
+  if (empile==TRUE){p = p + facet_wrap( ~ modalite, ncol =1)}
+  if (length(mel) > 0 & length(mel) < nrow(Data) & empile == FALSE) {p = p + annotate("text",label = rep("*",length(mel)),x=mel,y=0, size=10)}
   p = p + ggtitle(paste(person, variable,nom,  sep=" : "))
-  p = p + geom_vline(xintercept = Mean, size = 1.2, color="red") 
+  if (empile == FALSE) {
+    p = p + geom_vline(xintercept = Mean, size = 1.2, color="red")
+    p = p + geom_text(x=Mean,y=-0.03,label=paste("Gain moyen =",round((Mean)*100,2),"%",sep=" "), size=5)
+  }
   p = p + labs(x=paste("rapport Bouquet de sélection / Vrac pour ",variable,sep=""), y="Nombre de couple Bouquet - Vrac") 
-  p = p + geom_text(x=Mean,y=-0.03,label=paste("Gain moyen =",round((Mean)*100,2),"%",sep=" "), size=5)
   p = p + geom_vline(xintercept = 0,  linetype = "dotted")
   p = p + scale_fill_discrete(name = "Significativité")
+
  # p = p +annotate("text",label = "* : Sélection(s) dans le mélange",x=(max(Data$overyielding)-0.5),y=3)
   
   return(list("histo" = p, "tab" = Data))
