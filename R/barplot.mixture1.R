fd3a9c6094a5c3da917d6c3937b046a852844fe6
gaelleVF <gaelle.van.frank@gmail.com>
2016-11-09 09:58:35 +0100

    correct barplot title

--- R/barplot.mixture1.R
+++ R/barplot.mixture1.R
@@ -2,7 +2,9 @@
 #' Generates barplots for mixtures
 #' 
 #' @param x data frame containing the name of the populations, the median of the character, the type ("Mixture","Component",...)
-#
+#'
+#' @param title title of the plot
+#' 
 #' @return The boxplot
 #' 
 #' @author Gaelle Van Frank
@@ -10,13 +12,13 @@
 #' @seealso \code{\link{PPBformations::ggplot_mixture1}}
 #' 
 
-barplot.mixture1 = function(x) {
+barplot.mixture1 = function(x, title) {
   
   p = ggplot(x, aes(x = reorder(germplasm, median), y = median, fill=unlist(x$type))) + geom_bar(stat = "identity")+ theme(legend.title = element_blank())
   
   # ajouter les groupes de significativité
   p = p + geom_text(data = x, aes(x = reorder(germplasm, median), y = median/2, label = groups), angle = 90, color = "white")
-  p = p + ggtitle(paste(x[1, "environment"], "\n alpha = ", x[1, "alpha"], "; alpha correction :", x[1, "alpha.correction"])) + ylab("")
+  p = p + ggtitle(title) + ylab("")
   
   # pivoter légende axe abscisses
   p = p + xlab("") + theme(axis.text.x = element_text(angle = 90)) + ylim(0, x[1,"max"])
