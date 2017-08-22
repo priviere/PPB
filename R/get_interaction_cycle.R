4c31f515a3ba6611cea5f3723db3c9d36d296470
gaelleVF <gaelle.van.frank@gmail.com>
2017-08-22 16:03:42 +0200

    return NULL if there is data for only 1 variable

--- R/get_interaction_cycle.R
+++ R/get_interaction_cycle.R
@@ -16,33 +16,34 @@ get_interaction_cycle <- function(data,
 	
   a = grep(paste(paste("^",vec_variables,"$",sep=""),collapse="|"),colnames(d))
   var_not_in_data = setdiff(vec_variables,colnames(d)[a])
-  if(length(var_not_in_data)>0){vec_variables = vec_variables[-grep(var_not_in_data,vec_variables)]}
-  variable= gsub("^([^---]*)---.*$", "\\1",	vec_variables)
-  colnames(d)[a] =variable
-  
-	q=melt(d,id.vars="son",measure.vars=variable)
-	q=q[order(q$son),]
-	if(!is.null(nb_parameters_per_plot_in.col)){
-		ns = unique(q$son)
-		s = rep(c(1:length(ns)), each = nb_parameters_per_plot_in.col)[1:length(ns)]
-		names(s) = ns
-		q$split_in.col = s[q$son]
-	} else { q$split_in.col = rep(1, nrow(q)) }
-	q$split =paste("son", q$split_in.col, sep = "-")
-	if(equal.ylim){ # Set ylim for all ggplots
-		y.lim = max(q$value)
-	}
-	q =  plyr:::splitter_d(q, .(split))
-	
-	p = lapply(q,function(data){
-		p = ggplot(data, aes(y = value, x = factor(variable), colour = factor(son), group = factor(son))) 
-		if(equal.ylim){p = p + coord_cartesian(ylim = c(0.5,as.numeric(y.lim)))}
-		p = p + stat_summary(fun.y = mean, geom = "line") + stat_summary(fun.y = mean, geom = "point") 
-		p = p + labs(x="Evolution des notes",y="notes globales")
-		p = p + labs(color="population")
-		return(p)
-	})
-
+  if(length(var_not_in_data)!=2){
+    if(length(var_not_in_data)==1){vec_variables = vec_variables[-grep(var_not_in_data,vec_variables)]}
+    variable= gsub("^([^---]*)---.*$", "\\1",	vec_variables)
+    colnames(d)[a] =variable
+    
+  	q=melt(d,id.vars="son",measure.vars=variable)
+  	q=q[order(q$son),]
+  	if(!is.null(nb_parameters_per_plot_in.col)){
+  		ns = unique(q$son)
+  		s = rep(c(1:length(ns)), each = nb_parameters_per_plot_in.col)[1:length(ns)]
+  		names(s) = ns
+  		q$split_in.col = s[q$son]
+  	} else { q$split_in.col = rep(1, nrow(q)) }
+  	q$split =paste("son", q$split_in.col, sep = "-")
+  	if(equal.ylim){ # Set ylim for all ggplots
+  		y.lim = max(q$value)
+  	}
+  	q =  plyr:::splitter_d(q, .(split))
+  	
+  	p = lapply(q,function(data){
+  		p = ggplot(data, aes(y = value, x = factor(variable), colour = factor(son), group = factor(son))) 
+  		if(equal.ylim){p = p + coord_cartesian(ylim = c(0.5,as.numeric(y.lim)))}
+  		p = p + stat_summary(fun.y = mean, geom = "line") + stat_summary(fun.y = mean, geom = "point") 
+  		p = p + labs(x="Evolution des notes",y="notes globales")
+  		p = p + labs(color="population")
+  		return(p)
+  	})
+  }else{p=NULL}
   return(p)
 }
 
