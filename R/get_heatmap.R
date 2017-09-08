d1f3ba07e9b5dfd10f56743a58831da6ace74f6b
gaelleVF <gaelle.van.frank@gmail.com>
2017-09-08 11:57:25 +0200

    leave all years if splited graph + keep present year first

--- R/get_heatmap.R
+++ R/get_heatmap.R
@@ -11,19 +11,22 @@ get_heatmap <- function(data,
   colnames(HM)[3] = "var"
   HM=HM[!(HM$var == "NA"),]
   HM$var = factor(HM$var)
-  
+  HM$son_year = factor(HM$son_year)
   if(vec_variables == "verse---verse"){HM$var = factor(HM$var,levels(HM$var)[c(3,5,4,2,1)])}
   
   HM$son = unlist(lapply(as.character(HM$son),function(x){strsplit(x,"_")[[1]][1]}))
+  HM=HM[order(as.numeric(as.character(HM$son_year)),decreasing=TRUE),]
+  
   split = rep(seq(1,100,1),nb_parameters_per_plot)
   split = split[order(split)]
   split=split[1:length(unique(HM$son))]
   names(split)=unique(HM$son)
+  
   HM$split = split[HM$son]
   to_plot =  plyr:::splitter_d(HM, .(split))
-  
   p = lapply(to_plot,function(data){
     p = ggplot(data=data,aes(x=son_year,y=son,fill=var)) + geom_tile() + scale_fill_manual(drop=FALSE,values =c("chartreuse4","seagreen2","palegreen1","coral2","red3"))
+    p = p +  scale_x_discrete(drop=FALSE)
     p = p + labs(x = "année", y="populations", fill=paste("note",vec_variables,sep=" "))
   })
 
