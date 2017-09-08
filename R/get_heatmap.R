e8d24aca7b08af0bbf6864b6e5bc86b912e78037
gaelleVF <gaelle.van.frank@gmail.com>
2017-09-08 09:48:25 +0200

    put words instead of letters for verse

--- R/get_heatmap.R
+++ R/get_heatmap.R
@@ -12,17 +12,18 @@ get_heatmap <- function(data,
   HM=HM[!(HM$var == "NA"),]
   HM$var = factor(HM$var)
   
+  if(vec_variables == "verse---verse"){HM$var = factor(HM$var,levels(HM$var)[c(3,5,4,2,1)])}
+  
   HM$son = unlist(lapply(as.character(HM$son),function(x){strsplit(x,"_")[[1]][1]}))
   split = rep(seq(1,100,1),nb_parameters_per_plot)
   split = split[order(split)]
   split=split[1:length(unique(HM$son))]
   names(split)=unique(HM$son)
   HM$split = split[HM$son]
-  if(vec_variables == "verse"){levels(HM) = c(1,2,3,4,5)}
   to_plot =  plyr:::splitter_d(HM, .(split))
   
   p = lapply(to_plot,function(data){
-    p = ggplot(data=data,aes(x=son_year,y=son,fill=var)) + geom_tile() + scale_fill_manual(drop=FALSE,values =c("red3","coral2","palegreen1","seagreen2","chartreuse4"))
+    p = ggplot(data=data,aes(x=son_year,y=son,fill=var)) + geom_tile() + scale_fill_manual(drop=FALSE,values =c("chartreuse4","seagreen2","palegreen1","coral2","red3"))
     p = p + labs(x = "année", y="populations", fill=paste("note",vec_variables,sep=" "))
   })
 
